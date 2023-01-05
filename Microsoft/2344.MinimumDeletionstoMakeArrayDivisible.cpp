0eb4473b922c58cc9ca674877fcbc49c98436510
MdIfham <76588472+MdIfham@users.noreply.github.com>
2023-01-05 22:34:10 +0530

    Create 2344.MinimumDeletionstoMakeArrayDivisible.cpp
    // 2344. Minimum Deletions to Make Array Divisible
    // Link: https://leetcode.com/problems/minimum-deletions-to-make-array-divisible/submissions/

---
+++ Microsoft/2344.MinimumDeletionstoMakeArrayDivisible.cpp
@@ -0,0 +1,29 @@
+// 2344. Minimum Deletions to Make Array Divisible
+// Link: https://leetcode.com/problems/minimum-deletions-to-make-array-divisible/submissions/
+
+class Solution {
+private:
+    bool isDividing(int n,vector<int>& numsDivide){
+        for(auto it:numsDivide){
+            if(it%n!=0) return false;
+        }
+        return true;
+    }
+public:
+    int minOperations(vector<int>& nums, vector<int>& numsDivide) {
+        map<int,int> mp;
+        for(auto it:nums)mp[it]++; 
+        int del=0;
+        bool flag=false;
+        for(auto it :mp){
+            if(isDividing(it.first,numsDivide)){
+                flag=true;
+                break;
+            }
+            else
+                del+=it.second;
+        }
+        if(flag) return del;
+        return -1;
+    }
+};
