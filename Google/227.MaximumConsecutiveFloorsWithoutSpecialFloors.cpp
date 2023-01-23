4a15cf8574ad4172bd29feed8bdeed90a545ebc5
MdIfham <76588472+MdIfham@users.noreply.github.com>
2023-01-23 21:10:19 +0530

    Update 227.MaximumConsecutiveFloorsWithoutSpecialFloors.cpp

--- Google/227.MaximumConsecutiveFloorsWithoutSpecialFloors.cpp
+++ Google/227.MaximumConsecutiveFloorsWithoutSpecialFloors.cpp
@@ -8,16 +8,11 @@ public:
         sort(special.begin(), special.end());
         int ans = 0;
         
-        if(bottom != special[0]) {
-            ans = max(ans, special[0]-bottom);
-        }
+        ans = max(ans, special[0]-bottom);
+        ans = max(ans, top-special[n-1]);
         
         for(int i=1; i<n; i++) {
             ans = max(ans, special[i]-special[i-1]-1);
-        }
-        
-        if(top != special[n-1]) {
-            ans = max(ans, top-special[n-1]);
         }
         
         return ans;
