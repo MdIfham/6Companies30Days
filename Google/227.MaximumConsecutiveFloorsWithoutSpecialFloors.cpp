e095f9bd58d7cba0b15bef684a6b2c467d3d9c9b
MdIfham <76588472+MdIfham@users.noreply.github.com>
2023-01-23 21:06:44 +0530

    Update 227.MaximumConsecutiveFloorsWithoutSpecialFloors.cpp

--- Google/227.MaximumConsecutiveFloorsWithoutSpecialFloors.cpp
+++ Google/227.MaximumConsecutiveFloorsWithoutSpecialFloors.cpp
@@ -1,3 +1,25 @@
 // 2274. Maximum Consecutive Floors Without Special Floors
 // https://leetcode.com/problems/maximum-consecutive-floors-without-special-floors/
 
+class Solution {
+public:
+    int maxConsecutive(int bottom, int top, vector<int>& special) {
+        int n = special.size();
+        sort(special.begin(), special.end());
+        int ans = 0;
+        
+        if(bottom != special[0]) {
+            ans = max(ans, special[0]-bottom);
+        }
+        
+        for(int i=1; i<n; i++) {
+            ans = max(ans, special[i]-special[i-1]-1);
+        }
+        
+        if(top != special[n-1]) {
+            ans = max(ans, top-special[n-1]);
+        }
+        
+        return ans;
+    }
+};
